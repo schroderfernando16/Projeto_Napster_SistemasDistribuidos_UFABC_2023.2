2ad4b7dfb7ef24444e1e22b13dfbddb5ecf4cd3c
schroderfernando16 <100815815+schroderfernando16@users.noreply.github.com>
2023-06-24 15:54:36 -0300

    Update serverv5.py

--- serverv5.py
+++ serverv5.py
@@ -1,29 +1,35 @@
+#NOME: Fernando Schroder Rodrigues
+#RA: 11201921885
+
 import socket
 
+'''Aqui definimos toda a classe de servidor, deixando ele habilitado para receber e armazenar os códigos dos peers'''
 class NapsterServer:
     def __init__(self, server_ip, server_port):
         self.server_ip = server_ip
         self.server_port = server_port
         self.peers = {}
-
+#Função JOIN, o servidor recebe as informações do peer que vai se juntar a rede recebendo, a porta, o Ip e a Pasta que será do peer. 
     def join(self, peer_socket, peer_address, peer_name, files, peer_port):
         self.peers[peer_address] = {'name': peer_name, 'files': files, 'port': peer_port}
-        print(f'Sou peer {peer_address}:{peer_port} com arquivos {files}')
-
+        print(f' Peer {peer_address} adicionado com arquivos {files}')
+#Função SEARCH, Recebe um arquvo onde ele irá procurar dentro do dicionario de peers qual peer possui o arquivo desejado.
     def search(self, search_query):
         response = ''
         for peer_address, peer_info in self.peers.items():
             files = peer_info['files']
             if search_query in files:
-                response += f'{peer_address} '
+                response += f'{peer_address}'
         return response.strip()
-    
+#Função UPDATE, Recebe o arquivo, e atualiza o indice com o novo arquivo baixado    
     def update_peer_files(self, peer_address, file_name):
         if peer_address in self.peers:
             self.peers[peer_address].append(file_name)
         else:
             self.peers[peer_address] = [file_name]
 
+
+#Função DOWNLOAD, pega o resultado da busca e pede o IP e a Porta em que será feito o Download, em caso de erro o servidor manda mensagem para o peer sobre o erro
     def download(self, peer_address, file_name):
         peer_ip, peer_port = peer_address.split(':')
         peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -43,12 +49,11 @@ class NapsterServer:
                         if not data:
                             break
                         file.write(data)
-                print(f'Arquivo {file_name} baixado do peer {peer_address} e salvo em {file_path}')
         except Exception as e:
             print(f'Erro ao conectar ao peer {peer_address}: {str(e)}')
         finally:
             peer_socket.close()
-
+#Função para fazer o gerenciamento das conexões, foi inspirada em páginas da internet e foruns de dúvidas online (link para o site está no relatório)
     def handle_peer(self, peer_socket, peer_address):
         message = peer_socket.recv(1024).decode()
         message_parts = message.split()
@@ -62,12 +67,12 @@ class NapsterServer:
             response = 'JOIN_OK'
         elif command == 'SEARCH':
             search_query = message_parts[1]
+            print(f'Peer {message_parts[2]} solicitou arquivo {search_query}')
             response = self.search(search_query)
         elif command == 'UPDATE':
                 file_name = message_parts[1]
                 self.update_peer_files(peer_address, file_name)
                 response = 'UPDATE_OK'
-                print(response)
         elif command == 'DOWNLOAD':
             file_name = message_parts[1]
             if peer_address in self.peers:
@@ -81,12 +86,12 @@ class NapsterServer:
         peer_socket.send(response.encode())
         peer_socket.close()
 
+#Main para inicializar o servidor
 def main():
     server = NapsterServer('localhost', 5000)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((server.server_ip, server.server_port))
     server_socket.listen(5)
-    print(f'Servidor iniciado em {server.server_ip}:{server.server_port}')
 
     while True:
         peer_socket, peer_address = server_socket.accept()
